022a87ae44fc4cf6cf085698f6ff8cfeec85aad8
鈴木瑛二 <suzukieiji@suzukieijinomacbook-air.local>
2017-08-14 02:02:58 +0900

    a

--- TabSample/FirstViewController.swift
+++ TabSample/FirstViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        #if DEBUG
+            print("debug")
+        #endif
+        var abcd = 0
+        print(abcd)
+        abcd = 1
+        print(calc(a: 1, b: 2))
+//        let a = CLLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,9 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func calc(a:Int, b:Int) -> Int {
+        return a + b
+    }
 
 }
 
